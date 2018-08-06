fdfd8f302f54cfe8a12dd0e0cf4f864795538fd0
Rui Santos <rvaldemar93@gmail.com>
2018-08-06 15:23:36 +0100

    models repo, user and collaboration created

---
+++ db/migrate/20180806142203_create_repos.rb
@@ -0,0 +1,10 @@
+class CreateRepos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :repos do |t|
+      t.references :user, foreign_key: true
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
