1c878c4399845786e1c32dcbbaf56c6f5ab5caf3
Rui Santos <rvaldemar93@gmail.com>
2018-08-07 19:15:38 +0100

    migration added to include name avatar nickname github and repos_url to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,17 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[github]
 
 def self.from_omniauth(auth)
+  raise
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+
     user.email = auth.info.email
     user.password = Devise.friendly_token[0,20]
-    #user.name = auth.info.name   # assuming the user model has a name
-    #user.image = auth.info.image # assuming the user model has an image
-    # If you are using confirmable and the provider(s) you use validate emails,
-    # uncomment the line below to skip the confirmation emails.
-    # user.skip_confirmation!
+    user.token = auth.credentials.token
+    user.avatar = auth.info.image
+    user.name = auth.info.name
+    user.nickname = auth.info.nickname
+    user.github = auth.info.GitHub
+    user.repos_url = auth.info.repos_url
   end
 end
 
