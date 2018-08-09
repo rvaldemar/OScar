134358c092331f1736ae3c388f2b6c52dd86377a
Rui Santos <40320519+rvaldemar@users.noreply.github.com>
2018-08-09 12:29:26 +0100

    variables oaut

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,18 +8,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: %i[github]
 
-def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
 
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
-    user.token = auth.credentials.token
-    user.avatar = auth.info.image
-    user.name = auth.info.name
-    user.nickname = auth.info.nickname
-    user.github = auth.info.GitHub
-    user.repos_url = auth.info.repos_url
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.token = auth.credentials.token
+      user.avatar = auth.info.image
+      user.name = auth.info.name
+      user.nickname = auth.info.nickname
+      user.github = auth.info.urls.GitHub
+      user.repos_url = auth[:extra].raw_info.repos_url
+    end
   end
-end
 
 end
